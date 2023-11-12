0bcc83ba857e21d18dd14f4a4e5dcde8465f7ff9
arya <yogaarya020@gmail.com>
2023-11-12 21:15:52 +0700

    perbaiki session

--- siswa/front/login_mhs/login_mhs.php
+++ siswa/front/login_mhs/login_mhs.php
@@ -5,9 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <link rel="stylesheet" href="../../back/css/login.css">
 </head>
 <body>
@@ -29,7 +28,8 @@ if (isset($_POST['login'])) {
     if ($result) {
         if (mysqli_num_rows($result) == 1) {
             // Mahasiswa ditemukan, set session dan arahkan ke halaman index.php
-            $_SESSION['mahasiswa'] = true;
+            $_SESSION['NIM'] = $nim;
+
             header("Location: ../sion.php");
             exit();
         } else {
--- siswa/front/sion.php
+++ siswa/front/sion.php
@@ -26,57 +26,61 @@
 	<link rel="stylesheet" href="assets/css/demo.css">
 </head>
 <body>
-<?php
 
-include "../koneksi.php";
-// Mulai sesi atau dapatkan sesi yang sudah ada
-session_start();
+<!-- kode php -->
+	<div>
+		<?php
 
-if (!isset($_SESSION['mahasiswa']) || $_SESSION['mahasiswa'] !== true) {
-	header("Location: login_mhs/login_mhs.php"); // Redirect ke halaman login jika belum login
-	exit();
-}
+		include "../koneksi.php";
+		// Mulai sesi atau dapatkan sesi yang sudah ada
+		session_start();
 
-// Set waktu timeout sesi dalam detik (contoh: 5 menit)
-$timeout = 300; // 5 menit * 60 detik
+		if (!isset($_SESSION['mahasiswa']) || $_SESSION['mahasiswa'] !== true) {
+			header("Location: login_mhs/login_mhs.php"); // Redirect ke halaman login jika belum login
+			exit();
+		}
 
-// Periksa apakah sesi terakhir lebih dari waktu timeout
-if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $timeout)) {
-    // Sesuaikan pesan sesuai kebutuhan
-    echo "Sesi Anda telah berakhir. Silakan login kembali.";
-    
-    // Hapus semua variabel sesi
-    session_unset();
+		// Set waktu timeout sesi dalam detik (contoh: 5 menit)
+		$timeout = 300; // 5 menit * 60 detik
 
-    // Hancurkan sesi
-    session_destroy();
+		// Periksa apakah sesi terakhir lebih dari waktu timeout
+		if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $timeout)) {
+				// Sesuaikan pesan sesuai kebutuhan
+				echo "Sesi Anda telah berakhir. Silakan login kembali.";
+				
+				// Hapus semua variabel sesi
+				session_unset();
 
-    // Redirect ke halaman login atau tindakan lainnya
-    header("Location: login_mhs/login_mhs.php");
-    exit();
-}
+				// Hancurkan sesi
+				session_destroy();
 
-// Perbarui waktu terakhir akses ke sesi
-$_SESSION['last_activity'] = time();
+				// Redirect ke halaman login atau tindakan lainnya
+				header("Location: login_mhs/login_mhs.php");
+				exit();
+		}
 
-// Ambil NIM dari sesi yang sudah login
-$nim = $_SESSION['mahasiswa'];
+		// Perbarui waktu terakhir akses ke sesi
+		$_SESSION['last_activity'] = time();
 
-// Query SELECT untuk menampilkan data mahasiswa berdasarkan NIM
-$query = "SELECT * FROM pendaftaran WHERE NIM = '$nim'";
-$result = mysqli_query($kon, $query);
-$data = mysqli_fetch_assoc($result);
-?>
+		// Ambil NIM dari sesi yang sudah login
+		$nim = $_SESSION['NIM'];
 
+		// Query SELECT untuk menampilkan data mahasiswa berdasarkan NIM
+		$query = "SELECT * FROM pendaftaran WHERE NIM = '$nim'";
+		$result = mysqli_query($kon, $query);
+		?>
+	</div>
 
+<!-- header -->
 	<div class="wrapper">
 		<div class="main-header">
 			<!-- Logo Header -->
 			<div class="logo-header" data-background-color="blue">
 				
-				<a href="index.html" class="logo">
-					<img src="assets/img/logo.svg" alt="navbar brand" class="navbar-brand">
-				</a>
+				<!-- <a href="index.html" class="logo" style="display: flex; justify-content:center; align-items:center;">
+					<img src="../back/css/logo1.png" width="70px">
+					<p style="margin-top: 20px; color:black;"><b>ITB STIKOM BALI</b></p>
+				</a> -->
 				<button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon">
 						<i class="icon-menu"></i>
@@ -316,6 +320,7 @@ $data = mysqli_fetch_assoc($result);
 			</nav>
 			<!-- End Navbar -->
 		</div>
+	<!-- end header -->
 
 		<!-- Sidebar -->
 		<div class="sidebar sidebar-style-2">			
@@ -329,10 +334,12 @@ $data = mysqli_fetch_assoc($result);
 							<a data-toggle="collapse" href="#collapseExample" aria-expanded="true">
 									<span>
 									<?php
-											// Menampilkan Nama dan NIM dari data mahasiswa
-											echo $data['NIM']; // Gantilah 'Nama' dengan nama kolom yang sesuai di database
-											echo "<span class='user-level'>" . $data['nama'] . "</span>";
-									?>
+									if ($data = mysqli_fetch_assoc($result)) {
+										// Menampilkan Nama dan NIM dari data mahasiswa
+										echo $data['NIM']; // Gantilah 'NIM' dengan nama kolom yang sesuai di database
+										echo "<span class='user-level'>" . $data['nama'] . "</span>";
+								}
+								?>
 									</span>
 							</a>
 							<div class="clearfix"></div>
@@ -386,6 +393,7 @@ $data = mysqli_fetch_assoc($result);
 		</div>
 		<!-- End Sidebar -->
 
+		<!-- content -->
 		<div class="main-panel">
 			<div class="content">
 				<div class="panel-header bg-primary-gradient">
@@ -544,150 +552,147 @@ $data = mysqli_fetch_assoc($result);
 			</div>
 
 		</div>
-		
-		<!-- Custom template | don't include it in your project! -->
-
-		<!-- End Custom template -->
+		<!-- End content -->
 	</div>
 
 
 
 	<!--   Core JS Files   -->
 	<div>
-	<script src="assets/js/core/jquery.3.2.1.min.js"></script>
-	<script src="assets/js/core/popper.min.js"></script>
-	<script src="assets/js/core/bootstrap.min.js"></script>
+		<script src="assets/js/core/jquery.3.2.1.min.js"></script>
+		<script src="assets/js/core/popper.min.js"></script>
+		<script src="assets/js/core/bootstrap.min.js"></script>
 
-	<!-- jQuery UI -->
-	<script src="assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
-	<script src="assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
+		<!-- jQuery UI -->
+		<script src="assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+		<script src="assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
 
-	<!-- jQuery Scrollbar -->
-	<script src="assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+		<!-- jQuery Scrollbar -->
+		<script src="assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
 
 
-	<!-- Chart JS -->
-	<script src="assets/js/plugin/chart.js/chart.min.js"></script>
+		<!-- Chart JS -->
+		<script src="assets/js/plugin/chart.js/chart.min.js"></script>
 
-	<!-- jQuery Sparkline -->
-	<script src="assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
+		<!-- jQuery Sparkline -->
+		<script src="assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
 
-	<!-- Chart Circle -->
-	<script src="assets/js/plugin/chart-circle/circles.min.js"></script>
+		<!-- Chart Circle -->
+		<script src="assets/js/plugin/chart-circle/circles.min.js"></script>
 
-	<!-- Datatables -->
-	<script src="assets/js/plugin/datatables/datatables.min.js"></script>
+		<!-- Datatables -->
+		<script src="assets/js/plugin/datatables/datatables.min.js"></script>
 
-	<!-- Bootstrap Notify -->
-	<script src="assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
+		<!-- Bootstrap Notify -->
+		<script src="assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
 
-	<!-- jQuery Vector Maps -->
-	<script src="assets/js/plugin/jqvmap/jquery.vmap.min.js"></script>
-	<script src="assets/js/plugin/jqvmap/maps/jquery.vmap.world.js"></script>
+		<!-- jQuery Vector Maps -->
+		<script src="assets/js/plugin/jqvmap/jquery.vmap.min.js"></script>
+		<script src="assets/js/plugin/jqvmap/maps/jquery.vmap.world.js"></script>
 
-	<!-- Sweet Alert -->
-	<script src="assets/js/plugin/sweetalert/sweetalert.min.js"></script>
+		<!-- Sweet Alert -->
+		<script src="assets/js/plugin/sweetalert/sweetalert.min.js"></script>
 
-	<!-- Atlantis JS -->
-	<script src="assets/js/atlantis.min.js"></script>
+		<!-- Atlantis JS -->
+		<script src="assets/js/atlantis.min.js"></script>
 
-	<!-- Atlantis DEMO methods, don't include it in your project! -->
-	<script src="assets/js/setting-demo.js"></script>
-	<script src="assets/js/demo.js"></script>
-	<script>
-		Circles.create({
-			id:'circles-1',
-			radius:45,
-			value:60,
-			maxValue:100,
-			width:7,
-			text: 5,
-			colors:['#f1f1f1', '#FF9E27'],
-			duration:400,
-			wrpClass:'circles-wrp',
-			textClass:'circles-text',
-			styleWrapper:true,
-			styleText:true
-		})
+		<!-- Atlantis DEMO methods, don't include it in your project! -->
+		<script src="assets/js/setting-demo.js"></script>
+		<script src="assets/js/demo.js"></script>
+		<script>
+			Circles.create({
+				id:'circles-1',
+				radius:45,
+				value:60,
+				maxValue:100,
+				width:7,
+				text: 5,
+				colors:['#f1f1f1', '#FF9E27'],
+				duration:400,
+				wrpClass:'circles-wrp',
+				textClass:'circles-text',
+				styleWrapper:true,
+				styleText:true
+			})
 
-		Circles.create({
-			id:'circles-2',
-			radius:45,
-			value:70,
-			maxValue:100,
-			width:7,
-			text: 36,
-			colors:['#f1f1f1', '#2BB930'],
-			duration:400,
-			wrpClass:'circles-wrp',
-			textClass:'circles-text',
-			styleWrapper:true,
-			styleText:true
-		})
+			Circles.create({
+				id:'circles-2',
+				radius:45,
+				value:70,
+				maxValue:100,
+				width:7,
+				text: 36,
+				colors:['#f1f1f1', '#2BB930'],
+				duration:400,
+				wrpClass:'circles-wrp',
+				textClass:'circles-text',
+				styleWrapper:true,
+				styleText:true
+			})
 
-		Circles.create({
-			id:'circles-3',
-			radius:45,
-			value:40,
-			maxValue:100,
-			width:7,
-			text: 12,
-			colors:['#f1f1f1', '#F25961'],
-			duration:400,
-			wrpClass:'circles-wrp',
-			textClass:'circles-text',
-			styleWrapper:true,
-			styleText:true
-		})
+			Circles.create({
+				id:'circles-3',
+				radius:45,
+				value:40,
+				maxValue:100,
+				width:7,
+				text: 12,
+				colors:['#f1f1f1', '#F25961'],
+				duration:400,
+				wrpClass:'circles-wrp',
+				textClass:'circles-text',
+				styleWrapper:true,
+				styleText:true
+			})
 
-		var totalIncomeChart = document.getElementById('totalIncomeChart').getContext('2d');
+			var totalIncomeChart = document.getElementById('totalIncomeChart').getContext('2d');
 
-		var mytotalIncomeChart = new Chart(totalIncomeChart, {
-			type: 'bar',
-			data: {
-				labels: ["S", "M", "T", "W", "T", "F", "S", "S", "M", "T"],
-				datasets : [{
-					label: "Total Income",
-					backgroundColor: '#ff9e27',
-					borderColor: 'rgb(23, 125, 255)',
-					data: [6, 4, 9, 5, 4, 6, 4, 3, 8, 10],
-				}],
-			},
-			options: {
-				responsive: true,
-				maintainAspectRatio: false,
-				legend: {
-					display: false,
-				},
-				scales: {
-					yAxes: [{
-						ticks: {
-							display: false //this will remove only the label
-						},
-						gridLines : {
-							drawBorder: false,
-							display : false
-						}
+			var mytotalIncomeChart = new Chart(totalIncomeChart, {
+				type: 'bar',
+				data: {
+					labels: ["S", "M", "T", "W", "T", "F", "S", "S", "M", "T"],
+					datasets : [{
+						label: "Total Income",
+						backgroundColor: '#ff9e27',
+						borderColor: 'rgb(23, 125, 255)',
+						data: [6, 4, 9, 5, 4, 6, 4, 3, 8, 10],
 					}],
-					xAxes : [ {
-						gridLines : {
-							drawBorder: false,
-							display : false
-						}
-					}]
 				},
-			}
-		});
+				options: {
+					responsive: true,
+					maintainAspectRatio: false,
+					legend: {
+						display: false,
+					},
+					scales: {
+						yAxes: [{
+							ticks: {
+								display: false //this will remove only the label
+							},
+							gridLines : {
+								drawBorder: false,
+								display : false
+							}
+						}],
+						xAxes : [ {
+							gridLines : {
+								drawBorder: false,
+								display : false
+							}
+						}]
+					},
+				}
+			});
 
-		$('#lineChart').sparkline([105,103,123,100,95,105,115], {
-			type: 'line',
-			height: '70',
-			width: '100%',
-			lineWidth: '2',
-			lineColor: '#ffa534',
-			fillColor: 'rgba(255, 165, 52, .14)'
-		});
-	</script>
+			$('#lineChart').sparkline([105,103,123,100,95,105,115], {
+				type: 'line',
+				height: '70',
+				width: '100%',
+				lineWidth: '2',
+				lineColor: '#ffa534',
+				fillColor: 'rgba(255, 165, 52, .14)'
+			});
+		</script>
 	</div>
 </body>
 </html>
